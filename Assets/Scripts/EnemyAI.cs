a9a812bc2fb4dac777459e3c5b23b92255d5b2d2
Fatma Beyzanur TOLAN <fbt.beyza@gmail.com>
2024-08-31 23:51:49 +0300

    enemy AI, navmesh

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+
+    public NavMeshAgent enemy;
+    public Transform Player;
+
+    void Update()
+    {
+        enemy.SetDestination(Player.position);
+    }
+
+}
