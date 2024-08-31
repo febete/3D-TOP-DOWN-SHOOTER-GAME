35f97e9e5206e8f4bcade9aa2148360aa30847e7
Fatma Beyzanur TOLAN <fbt.beyza@gmail.com>
2024-08-31 17:52:34 +0300

    added animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     //Handling
@@ -17,9 +18,17 @@ public class PlayerController : MonoBehaviour
 
     //Components
     private CharacterController controller;
+    private Animator animator;  // Animator bileşeni
+
+
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();  // Animator bileşenini al
+
+        animator.SetBool("isIdle", true);
+
     }
 
 
@@ -27,8 +36,11 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        if (input != Vector3.zero)
+
+        bool isMoving = input.magnitude > 0;
+        if (isMoving)
         {
+            Debug.Log("karakter hareket ediyor");
             targetRotation = Quaternion.LookRotation(input);
             transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
         }
@@ -41,6 +53,33 @@ public class PlayerController : MonoBehaviour
 
         motion += Vector3.up * -8;
         controller.Move(motion * Time.deltaTime);
+
+
+        //Animasyon parametleri ayarlama
+        if (isMoving)
+        {
+            animator.SetBool("isIdle", false);  // Hareket ediyorsa idle değil
+            animator.SetBool("isWalking", true);  // Hareket ediyorsa yürüyüş animasyonunu oynat
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);  // Hareket etmiyorsa yürüyüş animasyonunu durdur
+            animator.SetBool("isIdle", true);  // Hareket etmiyorsa idle animasyonunu oynat
+        }
+
+
+
+
+        // Boşluk tuşuna basılı olup olmadığını kontrol et
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Ateş Ediyor");
+            animator.SetBool("isFire", true);
+        }
+        else
+        {
+            animator.SetBool("isFire", false);
+        }
 
     }
 }
